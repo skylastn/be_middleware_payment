d0824a9b1ed81a3aa577682e06ebf3a694f1a226
skylastn <sahidrahutomo@gmail.com>
2022-12-03 00:03:33 +0700

    update 03/12/2022

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -17,17 +17,17 @@ class OrderController extends Controller
 {
     public function index(Request $request)
     {
-        try{
+        try {
             $cek        = (new ProjectController)->checkKey();
-            if(!$cek['status']){
+            if (!$cek['status']) {
                 return response()->json($cek, 403);
             }
             $page       = $request->page ?? "0";
             $limit      = $request->limit ?? "10";
             $response['message'] = "Success Get Order";
-            $response['data']   = Order::forPage($page,$limit)->where('type', $cek['data']->type)->get();
+            $response['data']   = Order::forPage($page, $limit)->where('type', $cek['data']->type)->get();
             return response()->json($response, 200);
-        }catch (\Exception $ex) {
+        } catch (\Exception $ex) {
             $error['line']      = $ex->getLine();
             $error['message']   = $ex->getMessage();
             $error['file']      = $ex->getFile();
@@ -37,42 +37,130 @@ class OrderController extends Controller
                 "message" => $ex->getMessage()
             ], 400);
         }
-        
     }
 
     public function store(Request $request)
-    {   
-        try{
-            // return $request->all();
-            $dateNow = date("Y-m-d H:i:s");
-            $cek        = (new ProjectController)->checkKey();
-            if(!$cek['status']){
-                return response()->json($cek, 403);
-            }
+    {
+        $project        = (new ProjectController)->checkKey();
+        if (!$project['status']) {
+            return response()->json($project, 403);
+        }
 
+        if ($project['data']->slug == "xendit") {
+            return $this->orderXendit($request, $project);
+        }
+        if ($project['data']->slug == "midtrans") {
+            return $this->orderXendit($request, $project);
+        }
+        $response['message']    = "Undefined Project";
+        return response()->json($response, 403);
+    }
+
+    public function orderMidtrans($request, $project)
+    {
+        $dateNow = date("Y-m-d H:i:s");
+        $date = date("Y-m-d");
+        try {
             DB::beginTransaction();
             $secretKey = Setting::where("key", "xendit_secretkey_sanbox")->first()->value;
-            if($request->mode == "prod"){
+            if ($request->mode == "prod") {
+                $secretKey = Setting::where("key", "xendit_secretkey_prod")->first()->value;
+            }
+            $urlSuccess = Setting::where("key", "url_success")->first()->value;
+            Xendit::setApiKey($secretKey);
+
+            $invID = DB::table('orders')->whereDate('created_at', $date)->orderBy('created_at', 'desc')->first();
+            $invIDCount                 = substr($invID->id ?? 00000, -5);
+            $invID_num                  = (int)$invIDCount + 1;
+            $merchantOrderId            = date("Ymd") . "-" . str_pad($invID_num, 5, '0', STR_PAD_LEFT);
+            // return $request->req;
+            $req['id']                  = $merchantOrderId;
+            $req['reference']           = $project['data']->type . '-' . $request->merchantOrderId;
+            $req['type']                = $project['data']->type;
+            $req['mode']                = $request->mode ?? "sandbox";
+            $transaction_details
+            
+            $params = [
+                "transaction_details"   =>,
+                "credit_card"           =>,
+                "customer_details"      =>,
+                // 'external_id' => $req['reference'] ?? $project['data']->type . '-' . $req['id'],
+                // 'amount' => $request->paymentAmount ?? 0,
+                // 'description' => $request->productDetails ?? "Payment",
+                // 'invoice_duration' => $expired,
+                // 'success_redirect_url' => $urlSuccess,
+                // 'failure_redirect_url' => $project['data']->callback,
+                // 'currency' => 'IDR',
+            ];
+
+            $req['request']             = json_encode($params);
+            $order                      = Order::create($req);
+
+            $dataLog['key'] = "request_order";
+            $dataLog['name'] = $req['request'];
+            $this->storeLog($project['data']->id, $dataLog);
+            
+            $createInvoice = \Xendit\Invoice::create($params);
+            $result = json_encode($createInvoice);
+            $dataLog['key'] = "request_order";
+            $dataLog['name'] = $result;
+            $this->storeLog($project['data']->id, $dataLog);
+
+            DB::table('orders')
+                ->where('id', $merchantOrderId)
+                ->limit(1)
+                ->update(
+                    [
+                        'response'      => $result,
+                        'updated_at'    => $dateNow,
+                        "url"           => $createInvoice['invoice_url']
+                    ]
+                );
+
+            DB::commit();
+            $response['message']    = "Success Create Order";
+            $response['link']       = $createInvoice['invoice_url'];
+            $response['data']       = $createInvoice;
+            return response()->json($response, 200);
+        } catch (\Exception $ex) {
+            $error['line']      = $ex->getLine();
+            $error['message']   = $ex->getMessage();
+            $error['file']      = $ex->getFile();
+            Log::error($error);
+            DB::rollback();
+            return response()->json([
+                "message" => $ex->getMessage()
+            ], 400);
+        }
+    }
+
+    public function orderXendit($request, $project)
+    {
+        try {
+            $dateNow = date("Y-m-d H:i:s");
+            DB::beginTransaction();
+            $secretKey = Setting::where("key", "xendit_secretkey_sanbox")->first()->value;
+            if ($request->mode == "prod") {
                 $secretKey = Setting::where("key", "xendit_secretkey_prod")->first()->value;
             }
             $urlSuccess = Setting::where("key", "url_success")->first()->value;
             Xendit::setApiKey($secretKey);
 
             $invID = DB::table('orders')->whereDate('created_at', Carbon::today())->orderBy('created_at', 'desc')->first();
-            $invIDCount                 = substr($invID->id ?? 00000,-5);
-            $invID_num                  = (int)$invIDCount+1;
-            $merchantOrderId            = date("Ymd"). "-" . str_pad($invID_num, 5, '0', STR_PAD_LEFT);
+            $invIDCount                 = substr($invID->id ?? 00000, -5);
+            $invID_num                  = (int)$invIDCount + 1;
+            $merchantOrderId            = date("Ymd") . "-" . str_pad($invID_num, 5, '0', STR_PAD_LEFT);
             // return $request->req;
             $req['id']                  = $merchantOrderId;
-            $req['reference']           = $cek['data']->type . '-' . $request->merchantOrderId;
-            $req['type']                = $cek['data']->type;
+            $req['reference']           = $project['data']->type . '-' . $request->merchantOrderId;
+            $req['type']                = $project['data']->type;
             $req['mode']                = $request->mode ?? "sandbox";
             $req['payment_method']      = "";
 
-            $expired                    = ($request->expiryPeriod ?? 0 ) * 60;
+            $expired                    = ($request->expiryPeriod ?? 0) * 60;
             // return $request;
-            $params = [ 
-                'external_id' => $req['reference'] ?? $cek['data']->type . '-' . $req['id'],
+            $params = [
+                'external_id' => $req['reference'] ?? $project['data']->type . '-' . $req['id'],
                 'amount' => $request->paymentAmount ?? 0,
                 'description' => $request->productDetails ?? "Payment",
                 'invoice_duration' => $expired,
@@ -120,7 +208,7 @@ class OrderController extends Controller
                 //     ]
                 // ],
                 'success_redirect_url' => $urlSuccess,
-                'failure_redirect_url' => $cek['data']->callback,
+                'failure_redirect_url' => $project['data']->callback,
                 'currency' => 'IDR',
                 // 'items' => [
                 //     [
@@ -138,38 +226,37 @@ class OrderController extends Controller
                 //     ]
                 // ]
             ];
-            
+
             $req['request']             = json_encode($params);
             $order                      = Order::create($req);
 
             $dataLog['key'] = "request_order";
             $dataLog['name'] = $req['request'];
-            $this->storeLog($cek['data']->id, $dataLog);
+            $this->storeLog($project['data']->id, $dataLog);
             // return $params;
             $createInvoice = \Xendit\Invoice::create($params);
             $result = json_encode($createInvoice);
             $dataLog['key'] = "request_order";
             $dataLog['name'] = $result;
-            $this->storeLog($cek['data']->id, $dataLog);
-            
-            
+            $this->storeLog($project['data']->id, $dataLog);
+
+
             DB::table('orders')
-                ->where('id',$merchantOrderId)
+                ->where('id', $merchantOrderId)
                 ->limit(1)
                 ->update(
-                [
-                    'response'      => $result,
-                    'updated_at'    => $dateNow,
-                    "url"           => $createInvoice['invoice_url']
-                ]
-            );
+                    [
+                        'response'      => $result,
+                        'updated_at'    => $dateNow,
+                        "url"           => $createInvoice['invoice_url']
+                    ]
+                );
 
             DB::commit();
             $response['message']    = "Success Create Order";
             $response['link']       = $createInvoice['invoice_url'];
             $response['data']       = $createInvoice;
             return response()->json($response, 200);
-
         } catch (\Exception $ex) {
             $error['line']      = $ex->getLine();
             $error['message']   = $ex->getMessage();
@@ -183,37 +270,37 @@ class OrderController extends Controller
     }
 
     public function Callback(Request $request)
-    {   
-        try{
+    {
+        try {
             DB::beginTransaction();
             $order = Order::where("reference", $request->external_id)->orderBy('id', 'DESC')->first();
-            if(!$order){
+            if (!$order) {
                 return response()->json([
                     "message" => "Order not found"
                 ], 200);
             }
 
             $xenditToken = Setting::where("key", "xendit_tokencallback_sanbox")->first()->value;
-            if($order->mode == "prod"){
+            if ($order->mode == "prod") {
                 $xenditToken = Setting::where("key", "xendit_tokencallback")->first()->value;
             }
             $reqHeaders = getallheaders();
             $incomingTokenXendit = isset($reqHeaders['X-Callback-Token']) ? $reqHeaders['X-Callback-Token'] : "";
-            
-            if($xenditToken != $incomingTokenXendit){
+
+            if ($xenditToken != $incomingTokenXendit) {
                 return response()->json([
                     "message" => "You are not permitted perform this"
                 ], 403);
             }
 
-            
+
             $order->callback        = json_encode($request->all());
             $order->status          = $request->status;
 
             $order->payment_method  = $request->payment_channel;
-            
+
             $order->save();
-            
+
 
 
             $dataLog['key']     = "callback_order";
@@ -221,19 +308,18 @@ class OrderController extends Controller
             $split              = explode("-", $request->external_id);
             $project            = Project::where("type", $split[0])->first();
             $this->storeLog($project->id, $dataLog);
-            if($request->status == "PAID"){
+            if ($request->status == "PAID") {
                 $params['merchantOrderId']  = $split[1] . "-" . $split[2];
                 $params['paymentCode']      = $order->payment_method;
                 $params['resultCode']       = "00";
-                $callback                   = $this->sendCallback($project->value, $params , $project->callback);
+                $callback                   = $this->sendCallback($project->value, $params, $project->callback);
             }
-            
+
             DB::commit();
             $response['message']    = "Success Send Callback";
             $response['data']       = $callback;
-            
-            return response()->json($response, 200);
 
+            return response()->json($response, 200);
         } catch (\Exception $ex) {
             $error['line']      = $ex->getLine();
             $error['message']   = $ex->getMessage();
@@ -246,7 +332,8 @@ class OrderController extends Controller
         }
     }
 
-    public function sendCallback($token, $params, $urlCallback){
+    public function sendCallback($token, $params, $urlCallback)
+    {
 
         $curl = curl_init();
 
@@ -259,7 +346,7 @@ class OrderController extends Controller
             CURLOPT_FOLLOWLOCATION => true,
             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
             CURLOPT_CUSTOMREQUEST => 'POST',
-            CURLOPT_POSTFIELDS =>json_encode($params),
+            CURLOPT_POSTFIELDS => json_encode($params),
             CURLOPT_HTTPHEADER => array(
                 "Token: $token",
                 'Content-Type: application/json'
@@ -277,7 +364,7 @@ class OrderController extends Controller
     }
 
     public function storeLog($id, $dataLog)
-    {   
+    {
         $dateNow                    = date("Y-m-d H:i:s");
         $dataLog['ip']              = $this->getClientIP();
         $dataLog['created_at']      = $dateNow;
@@ -286,24 +373,23 @@ class OrderController extends Controller
         return $insertLog;
     }
 
-    public function getClientIP() {
+    public function getClientIP()
+    {
         $ipaddress = '';
         if (getenv('HTTP_CLIENT_IP'))
             $ipaddress = getenv('HTTP_CLIENT_IP');
-        else if(getenv('HTTP_X_FORWARDED_FOR'))
+        else if (getenv('HTTP_X_FORWARDED_FOR'))
             $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
-        else if(getenv('HTTP_X_FORWARDED'))
+        else if (getenv('HTTP_X_FORWARDED'))
             $ipaddress = getenv('HTTP_X_FORWARDED');
-        else if(getenv('HTTP_FORWARDED_FOR'))
+        else if (getenv('HTTP_FORWARDED_FOR'))
             $ipaddress = getenv('HTTP_FORWARDED_FOR');
-        else if(getenv('HTTP_FORWARDED'))
-           $ipaddress = getenv('HTTP_FORWARDED');
-        else if(getenv('REMOTE_ADDR'))
+        else if (getenv('HTTP_FORWARDED'))
+            $ipaddress = getenv('HTTP_FORWARDED');
+        else if (getenv('REMOTE_ADDR'))
             $ipaddress = getenv('REMOTE_ADDR');
         else
             $ipaddress = 'UNKNOWN';
         return $ipaddress;
     }
-
-
 }
