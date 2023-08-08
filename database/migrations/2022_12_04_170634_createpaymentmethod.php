23f2f800b1391d2485f053333c8a3e672f813c3b
Sahid Rahutomo <sahid.rahutomo@digitalsekuriti.id>
2023-08-08 20:01:54 +0700

    update drop payment method

--- database/migrations/2022_12_04_170634_createpaymentmethod.php
+++ database/migrations/2022_12_04_170634_createpaymentmethod.php
@@ -31,6 +31,6 @@ return new class extends Migration
     public function down()
     {
         //
-        Schema::dropIfExists('payment_method');
+        Schema::dropIfExists('payment_methods');
     }
 };
