6088b948b93acf26aff19024ed9eb265f0c090a6
skylastn <sahidrahutomo@gmail.com>
2024-02-02 23:50:45 +0700

    Update SPNPay

--- app/Services/SPNPayService.php
+++ app/Services/SPNPayService.php
@@ -5,15 +5,32 @@ namespace App\Services;
 use App\Http\Helper\LogHelper;
 use App\Http\Helper\ResponseHelper;
 use App\Models\Order;
+use App\Models\Project;
+use App\Models\Setting;
 use Exception;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
-class DuitkuService
+class SPNPayService
 {
+    static function setEnv(string $mode): array
+    {
+        $result = array();
+        if ($mode == 'prod') {
+            $result['secretKey']    = Setting::where('key', 'spnpay_secretkey_sandbox')->first()->value ?? '';
+            $result['token']        = Setting::where('key', 'spnpay_token_sandbox')->first()->value ?? '';
+            $result['url']          = Setting::where('key', 'url_spnpay_sandbox')->first()->value ?? '';
+        } else {
+            $result['secretKey']    = Setting::where('key', 'spnpay_secretkey_prod')->first()->value ?? '';
+            $result['token']        = Setting::where('key', 'spnpay_token_prod')->first()->value ?? '';
+            $result['url']          = Setting::where('key', 'url_spnpay_prod')->first()->value ?? '';
+        }
+        // set sanitizer (default : true)
+        // $duitkuConfig->setSanitizedMode(false);
+        return $result;
+    }
     static function orderDuitku(Request $request, Project $project)
     {
-
-
         $dateNow = date("Y-m-d H:i:s");
         $date = date("Y-m-d");
 
@@ -32,72 +49,15 @@ class DuitkuService
             $req['mode']                        = $request->mode ?? "sandbox";
             $req['payment_method']              = $request->paymentMethod ?? '';
 
-            $paymentAmount      = $request->paymentAmount; // Amount
-            $email              = $request->email ?? "admin@ngudek.com"; // your customer email
-            $phoneNumber        = $request->phone ?? "081512356123"; // your customer phone number (optional)
-            $productDetails     = $request->productDetails;
-            $merchantOrderId    = $req['reference'] ?? $project->type . '-' . $req['id']; // from merchant, unique   
-            $additionalParam    = ''; // optional
-            $merchantUserInfo   = ''; // optional
-            $customerVaName     = $request->firstName ?? ""; // display name on bank confirmation display
-            $callbackUrl        = env('APP_URL') . '/api/callbackDuitku'; // url for callback
-            $returnUrl          = env('APP_URL') . '/api/callbackDuitku'; // url for redirect
-            $expiryPeriod       = $request->expiryPeriod ?? 180; // set the expired time in minutes
-
-            // Customer Detail
-            $firstName          = $request->firstName ?? "";
-            $lastName           = $request->lastName ?? "";
-
-            // Address
-            $alamat             = $request->address;
-            $city               = "Jakarta";
-            $postalCode         = "11530";
-            $countryCode        = "ID";
-
-            $address = array(
-                'firstName'     => $firstName,
-                'lastName'      => $lastName,
-                'address'       => $alamat,
-                'city'          => $city,
-                'postalCode'    => $postalCode,
-                'phone'         => $phoneNumber,
-                'countryCode'   => $countryCode
-            );
-
-            $customerDetail = array(
-                'firstName'         => $firstName,
-                'lastName'          => $lastName,
-                'email'             => $email,
-                'phoneNumber'       => $phoneNumber,
-                'billingAddress'    => $address,
-                'shippingAddress'   => $address
-            );
-
-            // Item Details
-            $item1 = array(
-                'name'      => $productDetails,
-                'price'     => $paymentAmount,
-                'quantity'  => 1
-            );
-
-            $itemDetails = array(
-                $item1
-            );
-
-            $params = array(
-                'paymentAmount'     => $paymentAmount,
-                'merchantOrderId'   => $merchantOrderId,
-                'productDetails'    => $productDetails,
-                'additionalParam'   => $additionalParam,
-                'merchantUserInfo'  => $merchantUserInfo,
-                'customerVaName'    => $customerVaName,
-                'email'             => $email,
-                'phoneNumber'       => $phoneNumber,
-                'itemDetails'       => $itemDetails,
-                'customerDetail'    => $customerDetail,
-                'callbackUrl'       => $callbackUrl,
-                'returnUrl'         => $returnUrl,
-                'expiryPeriod'      => $expiryPeriod
+            $params['bankCode']                 = '014';
+            $params['singleUse']                = true;
+            $params['type']                     = 'ClosedAmount';
+            $params['reference']                = $idSystem;
+            $params['amount']                   = $request->paymentAmount;
+            $params['expiryMinutes']            = 60;
+            $params['viewName']                 = $request->firstName ?? "AndalanSoftware";
+            $params['additionalInfo']           = array(
+                'callback' => env('APP_URL') . '/api/callbackSPNPay',
             );
 
             $req['request']         = json_encode($params);
@@ -110,22 +70,46 @@ class DuitkuService
                 'request_order_duitku'
             );
 
-            $duitkuConfig = DuitkuService::setEnv($request->mode);
-            // createInvoice Request
-            // $createInvoice = Pop::createInvoice($params, $duitkuConfig);
-            // $response = json_decode($createInvoice);
+            $config = SPNPayService::setEnv($request->mode);
+            $curl = curl_init();
+
+            curl_setopt_array($curl, array(
+                CURLOPT_URL => $config['url'] . '/virtual-account',
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => '',
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 0,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => 'POST',
+                CURLOPT_POSTFIELDS => json_encode($params),
+                CURLOPT_HTTPHEADER => array(
+                    'On-Key: ' . $config['secretKey'],
+                    'On-Token: ' . $config['token'],
+                    'On-Signature: ' . hash_hmac('sha512',  $config['secretKey'] + json_encode($params), $config['token']),
+                    'Accept: application/json',
+                    'Content-Type: application/json'
+                ),
+            ));
+
+            $$createInvoice = curl_exec($curl);
+
+            curl_close($curl);
+
+            $response = json_decode($createInvoice);
 
             LogHelper::sendLog(
-                'Response Order Duitku',
+                'Response Order SPNPay',
                 $response,
                 $project->id,
-                'response_order_duitku'
+                'response_order_spnpay'
             );
+
             $order->response        = json_encode($response);
             $order->url             = $response->paymentUrl;
             $order->save();
 
-            $msg                    = "Success Create Order Duitku";
+            $msg                    = "Success Create Order SPNPay";
             $result['link']         = $response->paymentUrl;
             $result['result']       = $response;
             DB::commit();
