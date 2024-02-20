6f7b7da6a89d31d17cbf7de88111a52fbfe92f59
Sahid Rahutomo <sahid.rahutomo@digitalsekuriti.id>
2024-02-20 09:36:06 +0700

    Fix Order SPNPay

--- app/Repository/SPNPayRepository.php
+++ app/Repository/SPNPayRepository.php
@@ -5,65 +5,65 @@ namespace App\Repository;
 
 class SPNPayRepository
 {
-    static function responseOrderFilter(array $data): array
+    static function responseOrderFilter(object $data): array
     {
-        $value['id']     = $data['id'] ?? '';
-        if (empty($data['virtualAccount'])) {
+        $value['id']     = $data->id ?? '';
+        if (empty($data->virtualAccount)) {
             array_merge($value, self::fillEmptyResponseOrder('virtualAccount', $data));
         }
-        if (empty($data['qris'])) {
+        if (empty($data->qris)) {
             array_merge($value, self::fillEmptyResponseOrder('qris', $data));
         }
-        if (empty($data['eWallet'])) {
+        if (empty($data->eWallet)) {
             array_merge($value, self::fillEmptyResponseOrder('eWallet', $data));
         }
-        if (empty($data['retail'])) {
+        if (empty($data->retail)) {
             array_merge($value, self::fillEmptyResponseOrder('retail', $data));
         }
-        if (empty($data['creditCard'])) {
+        if (empty($data->creditCard)) {
             array_merge($value, self::fillEmptyResponseOrder('creditCard', $data));
         }
         return $value;
     }
 
-    static function fillEmptyResponseOrder(string $type, array $data): array
+    static function fillEmptyResponseOrder(string $type, object $data): array
     {
         switch ($type) {
             case 'virtualAccount':
                 return array(
                     'virtualAccount' => array(
-                        'bankCode'      => $data['bankCode'] ?? '',
-                        'vaNumber'      => $data['vaNumber'] ?? '',
-                        'viewName'      => $data['viewName'] ?? '',
+                        'bankCode'      => $data->bankCode ?? '',
+                        'vaNumber'      => $data->vaNumber ?? '',
+                        'viewName'      => $data->viewName ?? '',
                     ),
                 );
             case 'qris':
                 return array(
                     'qris' => array(
-                        'content'       => $data['content'] ?? '',
-                        'url'           => $data['url'] ?? '',
+                        'content'       => $data->content ?? '',
+                        'url'           => $data->url ?? '',
                     ),
                 );
             case 'eWallet':
                 return array(
                     'eWallet' => array(
-                        'viewName'      => $data['viewName'] ?? '',
-                        'channel'       => $data['channel'] ?? '',
-                        'url'           => $data['url'] ?? '',
+                        'viewName'      => $data->viewName ?? '',
+                        'channel'       => $data->channel ?? '',
+                        'url'           => $data->url ?? '',
                     ),
                 );
             case 'retail':
                 return array(
                     'eWallet' => array(
-                        'viewName'      => $data['viewName'] ?? '',
-                        'channel'       => $data['channel'] ?? '',
-                        'paymentCode'   => $data['paymentCode'] ?? '',
+                        'viewName'      => $data->viewName ?? '',
+                        'channel'       => $data->channel ?? '',
+                        'paymentCode'   => $data->paymentCode ?? '',
                     ),
                 );
             case 'creditCard':
                 return array(
                     'creditCard' => array(
-                        'url'      => $data['url'] ?? '',
+                        'url'      => $data->url ?? '',
                     ),
                 );
             default:
