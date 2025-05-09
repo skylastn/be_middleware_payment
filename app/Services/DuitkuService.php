951d268621fc2dc8e1b7e5558ff58d1d7587117d
Sahid Rahutomo <sahidrahutomo@gmail.com>
2025-05-09 14:41:37 +0700

    Fix ordering

--- app/Services/DuitkuService.php
+++ app/Services/DuitkuService.php
@@ -52,11 +52,14 @@ class DuitkuService
 
             DB::beginTransaction();
 
-            $invID = DB::table('orders')->whereDate('created_at', $date)->orderBy('created_at', 'desc')->first();
+            $invID = DB::table('orders')->whereDate('created_at', $date)
+                ->orderBy('created_at', 'desc')
+                ->orderBy('id', 'desc')
+                ->first();
             $invIDCount                         = substr($invID->id ?? 00000, -5);
             $invID_num                          = (int)$invIDCount + 1;
             $idSystem                           = date("Ymd") . "-" . str_pad($invID_num, 5, '0', STR_PAD_LEFT);
-            dd($invID->id);
+            
             $req['id']                          = $idSystem;
             $req['reference']                   = $project->type . '-' . $request->merchantOrderId;
             $req['type']                        = $project->type;
