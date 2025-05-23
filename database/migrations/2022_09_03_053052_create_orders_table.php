5cf463a89cc20d4b563a15db29429c0173629fc3
Sahid Rahutomo <sahidrahutomo@gmail.com>
2025-05-24 01:31:46 +0700

    Fix unique table order

--- database/migrations/2022_09_03_053052_create_orders_table.php
+++ database/migrations/2022_09_03_053052_create_orders_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->string("id", 15);
             $table->string('type')->unique();
-            $table->text('reference')->unique();
+            $table->string('reference')->unique();
             $table->string('payment_method', 10)->nullable();
             $table->text('address')->nullable();
             $table->text('phone')->nullable();
